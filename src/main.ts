e8ae591a05ea7a1721168ca8aadcd73749b56d13
jmirabuenos-blip <Jmirabuenos@gbox.ncf.edu.ph>
2025-11-29 22:41:46 +0800

    Update backend

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule); 
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 5000;
   app.enableCors();
   await app.listen(+port);
   console.log(`🚀 Server running on http://localhost:${port}`);
